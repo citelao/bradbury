1d68fd514980427fbf60c2f6587f703ed72a85d3
Ben <ben@stolovitz.com>
2014-12-22 02:03:01 -0500

    much more generic

--- bradbury/header/math/vector.h
+++ bradbury/header/math/vector.h
@@ -2,6 +2,8 @@
 //  vector.h
 //  bradbury
 //
+//  An immutable, arbitrary length vector, designed for doubles, with typical matrix operations.
+//
 //  Created by Ben Stolovitz on 12/14/14.
 //  Copyright (c) 2014 citelao. All rights reserved.
 //
@@ -9,40 +11,183 @@
 #ifndef bradbury_vector_h
 #define bradbury_vector_h
 
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <initializer_list>
+#include <iostream>
+
+template<size_t D>
 class Vector {
 public:
-    Vector(double r) : _x(r), _y(r), _z(r) {};
-    Vector(double x, double y, double z) : _x(x), _y(y), _z(z) {};
+    // Default constructor
+    Vector() : Vector(0) {};
     
-    double x() const;
-    double y() const;
-    double z() const;
+    // Default value constructor
+    template <class T>
+    Vector(T r) {
+        _components = std::vector<double>(D, r);
+    };
+    
+    // Copy constructors
+    template <size_t E>
+    Vector(Vector<E> const &vector) {
+        throw std::length_error("Cannot initalize vector of size " + std::to_string(E) + " for dimension " + std::to_string(D));
+    }
+    Vector(Vector<D> const &vector) {
+        _components = vector._components;
+    };
+    
+    // List & vector constructors
+    template <class T>
+    Vector(std::initializer_list<T> components) {
+        if(components.size() != D) {
+            throw std::length_error("Cannot initalize vector of size " + std::to_string(components.size()) + " for dimension " + std::to_string(D));
+        }
+        
+        _components = std::vector<double>(components.begin(), components.end());
+    };
+    template <class T>
+    Vector(std::vector<T> components) {
+        if(components.size() != D) {
+            throw std::length_error("Cannot initalize vector of size " + std::to_string(components.size()) + " for dimension " + std::to_string(D));
+        }
+        
+        _components = std::vector<double>(components.begin(), components.end());
+    };
+    
+    // Specialized constructors for the first 4 dimensions.
+    template <class T>
+    Vector<2>(T x, T y) {
+        _components = {static_cast<double>(x), static_cast<double>(y)};
+    };
+    template <class T>
+    Vector<3>(T x, T y, T z) {
+        _components = {static_cast<double>(x), static_cast<double>(y), static_cast<double>(z)};
+    };
+    template <class T>
+    Vector<4>(T x, T y, T z, T t) {
+        _components = {static_cast<double>(x), static_cast<double>(y), static_cast<double>(z), static_cast<double>(t)};
+    };
+    
+    // Access operators
+    const double operator[](int i) const {
+        if(D <= i) {
+            throw std::out_of_range("no " + std::to_string(i) + " component for dimension " + std::to_string(D));
+        }
+        
+        return _components[i];
+    };
+    const double x() const {
+        return (*this)[0];
+    };
+    const double y() const {
+        if(D < 2) {
+            throw std::out_of_range("no y component for dimension " + std::to_string(D));
+        }
+        
+        return (*this)[1];
+    };
+    const double z() const {
+        if(D < 3) {
+            throw std::out_of_range("no z component for dimension " + std::to_string(D));
+        }
+        
+        return (*this)[2];
+    };
+    const double t() const {
+        if(D < 4) {
+            throw std::out_of_range("no z component for dimension " + std::to_string(D));
+        }
+        
+        return (*this)[3];
+    }
     
 //    double magnitude() const;
 //    double squaredmagnitude() const;
     
-    bool operator==(const Vector &nv) const;
-    bool operator!=(const Vector &nv) const;
+    const size_t dimension() const {
+        return D;
+    }
+    
+    bool operator==(const Vector &nv) const {
+        if(nv.dimension() != this->dimension())
+            return false;
+        
+        Vector diff = nv - *this;
+        
+        return (diff.x() < tolerance &&
+                diff.y() < tolerance &&
+                diff.z() < tolerance);
+    };
+    bool operator!=(const Vector &nv) const {
+        return !this->operator==(nv);
+    };
     
     // vector-vector operations
-    Vector operator+(const Vector &nv) const;
-    Vector operator-(const Vector &nv) const;
-    Vector operator-() const;
-    double operator*(const Vector &nv) const;
-    double dot(const Vector &nv) const;
-    Vector cross(const Vector &nv) const;
+    const Vector<D> operator+(const Vector<D> &nv) const {
+        std::vector<double> results = {};
+        for(int i = 0; i < _components.size(); i++) {
+            results.push_back(_components[i] + nv._components[i]);
+        }
+        
+        return Vector<D>(results);
+    };
+    const Vector<D> operator-(const Vector<D> &nv) const{
+        std::vector<double> results = {};
+        for(int i = 0; i < _components.size(); i++) {
+            results.push_back(_components[i] - nv._components[i]);
+        }
+        
+        return Vector<D>(results);
+    }
+    Vector<D> operator-() const {
+        std::vector<double> results = {};
+        for(int i = 0; i < _components.size(); i++) {
+            results.push_back(-_components[i]);
+        }
+        
+        return Vector<D>(results);
+    };
+    double operator*(const Vector &nv) const {
+        return this->dot(nv);
+    };
+    double dot(const Vector &nv) const {
+        return x() * nv.x() +
+        y() * nv.y() +
+        z() * nv.z();
+    };
+    Vector cross(const Vector &nv) const {
+        return Vector(y() * nv.z() - z() * nv.y(),
+                      - x() * nv.z() + z() * nv.x(),
+                      x() * nv.y() - y() * nv.x());
+    };
     
     // vector-number operations
-    Vector operator*(const double &d) const;
-    friend Vector operator*(const double &d, const Vector &v);
-    Vector operator/(const double &d) const;
+    Vector operator*(const double &d) const  {
+        return Vector(x() * d,
+                      y() * d,
+                      z() * d);
+    };
+    friend Vector operator*(const double &d, const Vector &v)  {
+        return v * d;
+    };
+    Vector operator/(const double &d) const {
+        return Vector(x() / d,
+                      y() / d,
+                      z() / d);
+    }
+;
     
 protected:
-    double _x;
-    double _y;
-    double _z;
+    std::vector<double> _components;
     
     double tolerance = 0.000001;
+    
+    template <typename Array>
+    void init(Array components) {
+        _components = components;
+    };
 };
 
 #endif // bradbury_vector_h
--- bradbury/tests/vector_test.cpp
+++ bradbury/tests/vector_test.cpp
@@ -7,73 +7,177 @@
 //
 
 #include "vector.h"
+#include <vector>
 
-TEST_CASE("vectors can be created with proper stats") {
-    SECTION("integer static example") {
-        Vector v = Vector(4, 5, 6);
-        REQUIRE(v.x() == 4);
-        REQUIRE(v.y() == 5);
-        REQUIRE(v.z() == 6);
+
+TEST_CASE("vectors can be created with proper stats", "[vector]") {
+    std::vector<double> traditionalStaticBase = {4, 6, 10, 12};
+    std::vector<Vector<4>> staticVectors = {
+        Vector<4>(4, 6, 10, 12),        // standard instantiation
+        {4, 6, 10, 12},                 // list instantiation
+        traditionalStaticBase           // std::vector instantiation
+    };
+    
+    double dx = rand() % 10000 / 100.0;
+    double dy = rand() % 10000 / 100.0;
+    double dz = rand() % 10000 / 100.0;
+    double dt = rand() % 10000 / 100.0;
+    std::vector<double> traditionalDynamicBase = {dx, dy, dz, dt};
+    std::vector<Vector<4>> dynamicVectors = {
+        Vector<4>(dx, dy, dz, dt),        // standard instantiation
+        {dx, dy, dz, dt},                 // list instantiation
+        traditionalDynamicBase            // std::vector instantiation
+    };
+    
+    SECTION("special dimensions init") {
+        Vector<1> d1(1);
+        Vector<2> d2(1, 2);
+        Vector<3> d3(1, 2, 3);
+        Vector<4> d4(1, 2, 3, 4);
+        
+        REQUIRE(d1.x() == 1);
+        REQUIRE(d2.x() == 1);
+        REQUIRE(d3.x() == 1);
+        REQUIRE(d4.x() == 1);
+        
+        REQUIRE_THROWS(d1.y());
+        REQUIRE(d2.y() == 2);
+        REQUIRE(d3.y() == 2);
+        REQUIRE(d4.y() == 2);
+        
+        REQUIRE_THROWS(d1.z());
+        REQUIRE_THROWS(d2.z());
+        REQUIRE(d3.z() == 3);
+        REQUIRE(d4.z() == 3);
+        
+        REQUIRE_THROWS(d1.t());
+        REQUIRE_THROWS(d2.t());
+        REQUIRE_THROWS(d3.t());
+        REQUIRE(d4.t() == 4);
     }
     
-    SECTION("integer dynamic example") {
-        int x = rand() % 100;
-        int y = rand() % 100;
-        int z = rand() % 100;
-        Vector v = Vector(x, y, z);
-        REQUIRE(v.x() == x);
-        REQUIRE(v.y() == y);
-        REQUIRE(v.z() == z);
+    SECTION("default value init") {
+        Vector<4> v(4.2);
+        REQUIRE(v.x() == 4.2);
+        REQUIRE(v.y() == 4.2);
+        REQUIRE(v.z() == 4.2);
+        REQUIRE(v.t() == 4.2);
+        
+        Vector<4> w;
+        REQUIRE(w.x() == 0);
+        REQUIRE(w.y() == 0);
+        REQUIRE(w.z() == 0);
+        REQUIRE(w.t() == 0);
     }
     
+    SECTION("wrong size protection") {
+        Vector<3> v;
+        
+        REQUIRE_THROWS_AS((v = {4, 6, 10, 12}), std::length_error);
+        REQUIRE_THROWS_AS((v = Vector<4>(4, 6, 10, 12)), std::length_error);
+        REQUIRE_THROWS_AS((v = traditionalStaticBase), std::length_error);
+        
+        std::vector<double> tooSmall = {4, 6};
+        REQUIRE_THROWS_AS((v = {4, 6}), std::length_error);
+        REQUIRE_THROWS_AS((v = Vector<2>(4, 6)), std::length_error);
+        REQUIRE_THROWS_AS((v = tooSmall), std::length_error);
+        
+        REQUIRE_NOTHROW(v = 4);
+    }
+    
+    SECTION("static example access") {
+        for(Vector<4> v : staticVectors) {
+            REQUIRE(v.x() == v[0]);
+            REQUIRE(v.y() == v[1]);
+            REQUIRE(v.z() == v[2]);
+            REQUIRE(v.t() == v[3]);
+            
+            REQUIRE_THROWS_AS(v[4], std::out_of_range);
+            
+            REQUIRE(v.x() == 4);
+            REQUIRE(v.y() == 6);
+            REQUIRE(v.z() == 10);
+            REQUIRE(v.t() == 12);
+        }
+        
+        Vector<1> v = {0};
+        REQUIRE_THROWS_AS(v[1], std::out_of_range);
+        REQUIRE_THROWS_AS(v[2], std::out_of_range);
+        REQUIRE_THROWS_AS(v[3], std::out_of_range);
+        REQUIRE_THROWS_AS(v.y(), std::out_of_range);
+        REQUIRE_THROWS_AS(v.z(), std::out_of_range);
+        REQUIRE_THROWS_AS(v.t(), std::out_of_range);
+    }
+    
+    SECTION("dynamic example access") {
+        for(Vector<4> v : dynamicVectors) {
+            REQUIRE(v.x() == v[0]);
+            REQUIRE(v.y() == v[1]);
+            REQUIRE(v.z() == v[2]);
+            REQUIRE(v.t() == v[3]);
+            
+            REQUIRE_THROWS_AS(v[4], std::out_of_range);
+            
+            REQUIRE(v.x() == dx);
+            REQUIRE(v.y() == dy);
+            REQUIRE(v.z() == dz);
+            REQUIRE(v.t() == dt);
+        }
+    }
+
     SECTION("double static example") {
-        Vector v = Vector(4.2, 5.2, 6.2);
+        Vector<3> v = Vector<3>(4.2, 5.2, 6.2);
         REQUIRE(v.x() == 4.2);
         REQUIRE(v.y() == 5.2);
         REQUIRE(v.z() == 6.2);
     }
-    
-    SECTION("one static number") {
-        double a = 4;
-        Vector v = Vector(a);
-        REQUIRE(v.x() == a);
-        REQUIRE(v.y() == a);
-        REQUIRE(v.z() == a);
-    }
-    
-    SECTION("one dynamic number") {
-        double a = rand() % 100;
-        Vector v = Vector(a);
-        REQUIRE(v.x() == a);
-        REQUIRE(v.y() == a);
-        REQUIRE(v.z() == a);
-    }
 }
 
-TEST_CASE("vectors can do math on each other") {
+TEST_CASE("vectors can do math on each other", "[vector]") {
     int ax = rand() % 100;
     int ay = rand() % 100;
     int az = rand() % 100;
-    Vector a = Vector(ax, ay, az);
+    Vector<3> a = {ax, ay, az};
     
     int bx = rand() % 100;
     int by = rand() % 100;
     int bz = rand() % 100;
-    Vector b = Vector(bx, by, bz);
+    Vector<3> b = {bx, by, bz};
+    
+    double cx = rand() % 10000 / 100.0;
+    double cy = rand() % 10000 / 100.0;
+    double cz = rand() % 10000 / 100.0;
+    double ct = rand() % 10000 / 100.0;
+    Vector<4> c = {cx, cy, cz, ct};
+    
+    double dx = rand() % 10000 / 100.0;
+    double dy = rand() % 10000 / 100.0;
+    double dz = rand() % 10000 / 100.0;
+    double dt = rand() % 10000 / 100.0;
+    Vector<4> d = {dx, dy, dz, dt};
     
     SECTION("can add") {
-        Vector v = a + b;
-        Vector w = b + a;
+        Vector<3> v = a + b;
+        Vector<3> w = b + a;
         
         REQUIRE(v == w);
         REQUIRE(v.x() == ax + bx);
         REQUIRE(v.y() == ay + by);
         REQUIRE(v.z() == az + bz);
+        
+        Vector<4> x = c + d;
+        Vector<4> y = d + c;
+        
+        REQUIRE(x == y);
+        REQUIRE(x.x() == cx + dx);
+        REQUIRE(x.y() == cy + dy);
+        REQUIRE(x.z() == cz + dz);
+        REQUIRE(x.t() == ct + dt);
     }
-    
+
     SECTION("can subtract") {
-        Vector v = a - b;
-        Vector w = b - a;
+        Vector<3> v = a - b;
+        Vector<3> w = b - a;
         
         REQUIRE(v != w);
         REQUIRE(v.x() == ax - bx);
@@ -83,78 +187,101 @@ TEST_CASE("vectors can do math on each other") {
         REQUIRE(w.x() == bx - ax);
         REQUIRE(w.y() == by - ay);
         REQUIRE(w.z() == bz - az);
+        
+        Vector<4> x = c - d;
+        Vector<4> y = d - c;
+        
+        REQUIRE(x != y);
+        REQUIRE(x.x() == cx - dx);
+        REQUIRE(x.y() == cy - dy);
+        REQUIRE(x.z() == cz - dz);
+        REQUIRE(x.t() == ct - dt);
+        
+        REQUIRE(y.x() == dx - cx);
+        REQUIRE(y.y() == dy - cy);
+        REQUIRE(y.z() == dz - cz);
+        REQUIRE(y.t() == dt - ct);
     }
     
     SECTION("can negate") {
-        Vector nega = -a;
+        Vector<3> nega = -a;
         
         REQUIRE(a.x() == -nega.x());
         REQUIRE(a.y() == -nega.y());
         REQUIRE(a.z() == -nega.z());
+        
+        Vector<4> negc = -c;
+        
+        REQUIRE(c.x() == -negc.x());
+        REQUIRE(c.y() == -negc.y());
+        REQUIRE(c.z() == -negc.z());
+        REQUIRE(c.t() == -negc.t());
     }
     
     SECTION("can static dot product") {
-        double dot = Vector(4, 5, 6) * Vector(7, 8, 9);
+        double dot = Vector<3>(4, 5, 6) * Vector<3>(7, 8, 9);
         
         REQUIRE(dot == 4 * 7 + 5 * 8 + 6 * 9);
+        
+        FAIL("need to test dot product in 4 dimensions");
     }
     
-    SECTION("can dynamic dot product") {
-        double ab = a * b;
-        double ba = b * a;
-        
-        // * is an alias for dot
-        double bdota = b.dot(a);
-        
-        REQUIRE(ab == ba);
-        REQUIRE(ab == bdota);
-        REQUIRE(ab == a.x() * b.x() +
-                    a.y() * b.y() +
-                    a.z() * b.z());
-    }
-    
-    SECTION("can static cross product") {
-        Vector cross = Vector(4, 5, 6).cross(Vector(7, 8, 9));
-        
-        REQUIRE(cross.x() == 5 * 9 - 6 * 8);
-        REQUIRE(cross.y() == - 4 * 9 + 6 * 7);
-        REQUIRE(cross.z() == 4 * 8 - 5 * 7);
-    }
-    
-    SECTION("can dynamic cross product") {
-        Vector ab = a.cross(b);
-        Vector ba = b.cross(a);
-        
-        REQUIRE(ab == -ba);
-        REQUIRE(ab.x() == ay * bz - by * az);
-        REQUIRE(ab.y() == - ax * bz + bx * az);
-        REQUIRE(ab.z() == ax * by - bx * ay);
-    }
+//    SECTION("can dynamic dot product") {
+//        double ab = a * b;
+//        double ba = b * a;
+//        
+//        // * is an alias for dot
+//        double bdota = b.dot(a);
+//        
+//        REQUIRE(ab == ba);
+//        REQUIRE(ab == bdota);
+//        REQUIRE(ab == a.x() * b.x() +
+//                    a.y() * b.y() +
+//                    a.z() * b.z());
+//    }
+//    
+//    SECTION("can static cross product") {
+//        Vector cross = Vector(4, 5, 6).cross(Vector(7, 8, 9));
+//        
+//        REQUIRE(cross.x() == 5 * 9 - 6 * 8);
+//        REQUIRE(cross.y() == - 4 * 9 + 6 * 7);
+//        REQUIRE(cross.z() == 4 * 8 - 5 * 7);
+//    }
+//    
+//    SECTION("can dynamic cross product") {
+//        Vector ab = a.cross(b);
+//        Vector ba = b.cross(a);
+//        
+//        REQUIRE(ab == -ba);
+//        REQUIRE(ab.x() == ay * bz - by * az);
+//        REQUIRE(ab.y() == - ax * bz + bx * az);
+//        REQUIRE(ab.z() == ax * by - bx * ay);
+//    }
 }
-
-TEST_CASE("vectors can do math with numbers") {
-    int x = rand() % 100;
-    int y = rand() % 100;
-    int z = rand() % 100;
-    Vector v = Vector(x, y, z);
-    double m = ((double)(rand() % 100)) / 10;
-    
-    SECTION("can multiply") {
-        Vector vm = v * m;
-        Vector mv = m * v;
-        
-        REQUIRE(vm == mv);
-        REQUIRE(vm.x() == x * m);
-        REQUIRE(vm.y() == y * m);
-        REQUIRE(vm.z() == z * m);
-    }
-    
-    SECTION("can divide") {
-        Vector vm = v / m;
-        
-        REQUIRE(vm.x() == x / m);
-        REQUIRE(vm.y() == y / m);
-        REQUIRE(vm.z() == z / m);
-    }
-}
-
+//
+//TEST_CASE("vectors can do math with numbers", "[vector]") {
+//    int x = rand() % 100;
+//    int y = rand() % 100;
+//    int z = rand() % 100;
+//    Vector v = Vector(x, y, z);
+//    double m = ((double)(rand() % 100)) / 10;
+//    
+//    SECTION("can multiply") {
+//        Vector vm = v * m;
+//        Vector mv = m * v;
+//        
+//        REQUIRE(vm == mv);
+//        REQUIRE(vm.x() == x * m);
+//        REQUIRE(vm.y() == y * m);
+//        REQUIRE(vm.z() == z * m);
+//    }
+//    
+//    SECTION("can divide") {
+//        Vector vm = v / m;
+//        
+//        REQUIRE(vm.x() == x / m);
+//        REQUIRE(vm.y() == y / m);
+//        REQUIRE(vm.z() == z / m);
+//    }
+//}
+//
